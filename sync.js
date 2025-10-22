820da2bb19b8d4e1279c788a138207af6d3d336e
fbossuet <fbossuet.pro@gmail.com>
2025-10-22 17:54:28 +0200

    Update sync.js

--- sync.js
+++ sync.js
@@ -1,4 +1,4 @@
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+import fetch from 'node-fetch';
 
 // ========================================
 // CONFIGURATION
