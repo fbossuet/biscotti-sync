e76057e6808d8749f2ee1dbc386cea8aa190f5e3
fbossuet <fbossuet.pro@gmail.com>
2025-12-03 19:59:10 +0400

    Rename 'ordre_d_affichage' to 'ordre-d-affichage'

--- sync.js
+++ sync.js
@@ -293,7 +293,7 @@ async function createWebflowItem(product) {
       'encart-vert': product.metafields.encartVert,
       'date-disponibilite': product.metafields.dateDisponibilite,
       'lien-vers-click-and-collect': product.metafields.lienversClickAndCollect,
-      'ordre_d_affichage' :product.metafields.ordreDaffichage
+      'ordre-d-affichage' :product.metafields.ordreDaffichage
     }
   };
 
@@ -344,7 +344,7 @@ async function updateWebflowItem(itemId, product) {
       'encart-vert': product.metafields.encartVert,
       'date-disponibilite': product.metafields.dateDisponibilite,
       'lien-vers-click-and-collect': product.metafields.lienversClickAndCollect,
-      'ordre_d_affichage' :product.metafields.ordreDaffichage
+      'ordre-d-affichage' :product.metafields.ordreDaffichage
     }
   };
 
